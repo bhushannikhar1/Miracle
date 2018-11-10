80432155a00a4239ec9a9e0746032f1f19a52017
bhushannikhar1 <bhushannikhar1@users.noreply.github.com>
2018-11-10 23:21:12 +0530

    Initial project setup

---
+++ Miracle/src/com/miracle/initialize/Initializer.java
@@ -0,0 +1,9 @@
+package com.miracle.initialize;
+
+import com.miracle.logic.Miracle;
+
+public class Initializer {
+	public static void main(String[] args) {
+		Miracle miracle= new Miracle();
+	}
+}
---
+++ Miracle/src/com/miracle/logic/Miracle.java
@@ -0,0 +1,14 @@
+package com.miracle.logic;
+
+
+/*	
+ * 	This class is responsible for Database
+ * 	@author : Bhushan Nikhar
+ * 
+ * */
+public class Miracle {
+	public Miracle()
+	{
+		System.out.println("Miracle Database");
+	}
+}
